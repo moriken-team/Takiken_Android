1acee56a2ed84c778dcf4d5b59bf89a0c1d7e911
takumi <you@example.com>
2015-05-19 23:29:07 +0900

    ドロップリストを作成

--- app/src/main/java/com/example/macuser/takiken/QuestionsAndAnswersFragment.java
+++ app/src/main/java/com/example/macuser/takiken/QuestionsAndAnswersFragment.java
@@ -14,8 +14,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.Arrays;
@@ -50,22 +53,51 @@ public class QuestionsAndAnswersFragment extends Fragment implements LoaderManag
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_questions_and_answers, container, false);
 
+        /* START ドロップダウンの表示設定 */
+        // Adapterの作成
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Adapterにアイテムを追加
+        adapter.add("滝沢のなりたち");
+        adapter.add("自然");
+        adapter.add("施設");
+        adapter.add("神社・仏閣");
+        adapter.add("伝統・文化");
+        adapter.add("交通");
+        adapter.add("人物");
+        adapter.add("イベント");
+        adapter.add("農作物・特産物");
+        adapter.add("生涯学習");
+        adapter.add("メディア");
 
+        Spinner spinner = (Spinner) view.findViewById(R.id.category);
+        // SpinnerにAdapterを設定
+        spinner.setAdapter(adapter);
+        /* END ドロップダウンの表示設定 */
 
-
-        //ボタンクリック：非同期処理により、入力した文字をログで表示
-        Button btn = (Button)view.findViewById(R.id.button);
+        //ボタンクリック：非同期処理により、入力した項目をPOSTで送信
+        Button btn = (Button) view.findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // レイアウトからSpinnerを取得
+                Spinner item = (Spinner) getActivity().findViewById(R.id.category);
+                // 選択したアイテム取得
+                String selected = (String) item.getSelectedItem();
+
+                // 選択したアイテムの位置を取得
+                int itemPosition = item.getSelectedItemPosition();
+
+                // ログで確認
+                Log.v("spinner item", selected);
+
+                String valueToString = String.valueOf(itemPosition);// 数値から文字列にキャスト変換
+                Log.v("spinner position", valueToString);
+
+
+
                 EditText question = (EditText) getActivity().findViewById(R.id.question);
                 EditText answer = (EditText) getActivity().findViewById(R.id.answer);
-
-//                // エディットテキストのテキストを設定します
-//                editText.setText("テスト");
-//                // エディットテキストのテキストを全選択します
-//                editText.selectAll();
-                // エディットテキストのテキストを取得します
 
                 String[] qa_result = new String[2];
 
@@ -147,6 +179,5 @@ public class QuestionsAndAnswersFragment extends Fragment implements LoaderManag
         // Loaderが参照しているデータを削除する
 
     }
-
     /* ---------- END LoaderCallback（非同期処理）コールバック処理 ---------- */
 }
